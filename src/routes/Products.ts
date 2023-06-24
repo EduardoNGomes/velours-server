16198c5a93824bdaee737e083c058c872014a585
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-06-24 13:49:41 -0300

    fix:Refact route products using jwt

--- src/routes/Products.ts
+++ src/routes/Products.ts
@@ -4,6 +4,13 @@ import { prisma } from '../lib/prisma'
 import { upload } from '../lib/multer'
 
 export async function ProductsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', async (request, reply) => {
+    try {
+      await request.jwtVerify()
+    } catch (err) {
+      reply.send(err)
+    }
+  })
   app.post(
     '/products/:user_id',
     { preHandler: upload.single('image') },
@@ -14,13 +21,9 @@ export async function ProductsRoutes(app: FastifyInstance) {
         price: z.coerce.number(),
       })
 
-      const paramsSchema = z.object({
-        user_id: z.string(),
-      })
-      const image = request.file
-
       const { price, description, name } = bodySchema.parse(request.body)
-      const { user_id } = paramsSchema.parse(request.params)
+      const { sub } = request.user
+      const image = request.file
 
       if (!image) {
         return reply.status(406).send('Please choose a valid image')
@@ -32,23 +35,35 @@ export async function ProductsRoutes(app: FastifyInstance) {
           coverUrl: image.filename,
           name,
           price,
-          userId: user_id,
+          userId: sub,
         },
       })
 
       return reply.send('Produto criado com sucesso')
     },
   )
+  app.get('/products/', async (request, reply) => {
+    const { sub } = request.user
 
-  app.get('/products/:user_id', async (request, reply) => {
-    const { user_id } = request.params
     const products = await prisma.products.findMany({
-      where: { userId: user_id },
+      where: { userId: sub },
     })
 
     return reply.status(200).send(products)
   })
+  app.get('/products/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+    const { sub } = request.user
 
+    const product = await prisma.products.findFirst({
+      where: { userId: sub, id },
+    })
+
+    return reply.status(200).send(product)
+  })
   app.get('/products/image/:image', async (request, reply) => {
     const paramsSchema = z.object({
       image: z.string(),
@@ -56,5 +71,53 @@ export async function ProductsRoutes(app: FastifyInstance) {
     const { image } = paramsSchema.parse(request.params)
 
     return reply.sendFile(image)
+  })
+  app.put(
+    '/products/:id',
+    { preHandler: upload.single('image') },
+    async (request, reply) => {
+      const bodySchema = z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        price: z.coerce.number().optional(),
+      })
+      const paramsSchema = z.object({
+        id: z.string(),
+      })
+      const { id } = paramsSchema.parse(request.params)
+      const { sub } = request.user
+      const { price, description, name } = bodySchema.parse(request.body)
+      const image = request.file
+
+      const product = await prisma.products.findUniqueOrThrow({
+        where: { id },
+      })
+
+      if (product.userId !== sub) {
+        throw new Error('Usuario invalido')
+      }
+
+      await prisma.products.update({
+        data: {
+          name: name || product.name,
+          description: description || product.description,
+          price: price || product.price,
+          coverUrl: image ? image.filename : product.coverUrl,
+        },
+        where: { id },
+      })
+
+      return reply.status(200).send('Produto atualizado com sucesso')
+    },
+  )
+  app.delete('/products/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.products.delete({ where: { id } })
+
+    return reply.status(200).send('Produto deletado com sucesso')
   })
 }
